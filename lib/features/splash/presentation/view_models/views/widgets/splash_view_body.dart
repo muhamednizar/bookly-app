a29b2db67388fd06c33f850bd43179f51169c467
midom <mohsennezar@gmail.com>
2025-01-28 20:16:24 +0200

    adding sliding animation in splash view

--- lib/features/splash/presentation/view_models/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view_models/views/widgets/splash_view_body.dart
@@ -1,22 +1,56 @@
+import 'package:bookly_app/features/splash/presentation/view_models/views/widgets/sliding_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../../../../core/utils/assets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        vsync: this,
+        duration: const Duration(
+          seconds: 1,
+        )
+    );
+    slidingAnimation = Tween<Offset>(begin: const Offset(0, 7),end:Offset.zero ).animate(animationController);
+    animationController.forward();
+
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-
       children: [
-        SvgPicture.asset(AssetsData.logo,width: 250,),
-         const SizedBox(height: 10,),
-         const Text('Read Free Books',textAlign: TextAlign.center,),
+        SvgPicture.asset(
+          AssetsData.logo,
+          width: 250,
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
 }
+
