74cbecc7acedfe9c90c64631051d2af5c7a47e9a
midom <mohsennezar@gmail.com>
2025-01-28 17:46:58 +0200

    splash body

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'features/splash/presentation/view_models/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplashView(),
+
+    );
+  }
+}
+
