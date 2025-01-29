6909bd592b019e7129726a2d4f7c9fc091f18afa
midom <mohsennezar@gmail.com>
2025-01-29 18:32:55 +0200

    Home screen top list view ui

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,8 +1,7 @@
-import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
+import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -14,25 +13,6 @@ class HomeViewBody extends StatelessWidget {
         CustomAppBar(),
         FeaturedBooksListView()
       ],
-    );
-  }
-}
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height *.3,
-
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal ,
-        itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8),
-          child: FeaturedListViewItem(),
-        );
-      },),
     );
   }
 }
